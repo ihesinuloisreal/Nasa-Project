70e8badaaeab17271a3c2159267976d102b86ebe
Ihesinulo Israel <ihesinuloisreali@gmail.com>
2024-08-07 05:09:02 +0100

    Test case added

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,25 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL = 'mongodb://127.0.0.1:27017/nasa'
 
 const app = require('./app');
 
 const {loadPlanetsData} = require('./models/planets.model')
 const server = http.createServer(app);
 
+mongoose.connection.once('open', ()=> {
+    console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (error)=>{
+    console.error(error)
+})
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
     
     app.listen(PORT, ()=>{
